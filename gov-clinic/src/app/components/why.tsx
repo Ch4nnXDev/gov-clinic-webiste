49e547ae2352498dbe0ff219692a403cef8053f1
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-02 14:41:44 +0530

    added justify to counter boxes

--- gov-clinic/src/app/components/why.tsx
+++ gov-clinic/src/app/components/why.tsx
@@ -48,15 +48,15 @@ export default function Why() {
 
         </div>
         <div className="flex flex-row  mt-10 mr-50 p-2 justify-center items-center text-black font-xl gap-3">
-          <div className="flex flex-col p-10">
+          <div className="flex flex-col p-10 justify-center items-center">
             <CountUp end={100} duration={6} prefix="+" className="text-4xl font-bold" />
             <span className="text-lg mt-5">Dedicated Professionals</span>
           </div>
-          <div className="flex flex-col p-10">
+          <div className="flex flex-col p-10 justify-center items-center">
             <CountUp end={1000} duration={6} prefix="+" className="text-4xl font-bold" />
             <span className="text-lg mt-5">Happy Patients</span>
           </div>
-          <div className="flex flex-col p-10">
+          <div className="flex flex-col p-10 justify-center items-center">
             <CountUp end={500} duration={6} prefix="+" className="text-4xl font-bold" />
             <span className="text-lg mt-5">Satisfied Patients</span>
           </div>
