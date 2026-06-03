ba25b3f062566b5de5dbd84d3b6ae98f8fc49396
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-03 10:26:59 +0530

    added the speed Insight Wrapper Component

--- gov-clinic/src/app/layout.tsx
+++ gov-clinic/src/app/layout.tsx
@@ -1,4 +1,5 @@
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import NavBar from "./components/nav-bar";
 import Footer from "./components/footer";
 import "./globals.css";
@@ -31,6 +32,7 @@ export default function RootLayout({
         </main>         
         <Footer />
         <Analytics />
+        <SpeedInsights />
           
         
         
