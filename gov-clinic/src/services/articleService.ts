0c781c9fdca8d05d5596f3f703766bb0d834a469
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-06 15:21:56 +0530

    fix

--- gov-clinic/src/services/articleService.ts
+++ gov-clinic/src/services/articleService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getAllArticles = async () => {
-    const response = await axios.get("/api/articles");
+    const response = await axios.get("http://localhost:4000/api/articles");
 
     return response.data;
 }
