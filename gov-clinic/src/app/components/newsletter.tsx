870ca87e4e414a79e12d43a2ab4f3906b1b9358d
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-06 15:21:42 +0530

    added the hook

--- gov-clinic/src/app/components/newsletter.tsx
+++ gov-clinic/src/app/components/newsletter.tsx
@@ -1,25 +1,22 @@
 'use client';
-import { useState, useEffect } from "react";
+import useGetArticles from "@/hooks/getArticles";
 import Image from "next/image";
-import axios from "axios";
+
 import { Scrollbar, Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 
+
 export default function Newsletter() {
-  const [articles, setArticles] = useState([]);
+  const { data } = useGetArticles();
   
   return (
     <section className="flex flex-col min-h-screen p-18">
       {/* Heading */}
       <div className="text-center">
         <h1 className="text-4xl font-bold text-gray-900">
-          Health Updates & Awareness
+      
         </h1>
-        <p className="text-lg text-gray-600 mt-2 max-w-2xl mx-auto">
-          Stay informed with the latest medical news, government initiatives, and
-          health education articles.
-        </p>
       </div>
 
       <hr className="mt-6 border-gray-300" />
@@ -38,8 +35,8 @@ export default function Newsletter() {
           }}
           modules={[Scrollbar, Navigation]}
         >
-          {articles.map((article, index) => (
-            <SwiperSlide key={index}>
+          {data?.articles?.map((article) => (
+            <SwiperSlide key={article.id}>
               <div className="bg-white rounded-xl shadow hover:shadow-md transition p-4">
                 <div className="relative w-full h-48">
                   <Image
@@ -47,13 +44,12 @@ export default function Newsletter() {
                     alt={article.title}
                     fill
                     className="object-cover rounded-lg"
-                    priority={index === 0} // preload first image
+                   
                   />
                 </div>
                 <h3 className="text-xl font-semibold text-gray-800 mt-4">
-                  {article.title}
+                  
                 </h3>
-                <p className="text-gray-600 mt-2 text-sm">{article.desc}</p>
                 <button className="mt-4 text-blue-600 hover:underline text-sm font-medium">
                   Read More →
                 </button>
