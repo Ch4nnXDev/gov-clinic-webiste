4549b4b08c4396b17e793940497005927abcc04a
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-07 07:19:21 +0530

    added bakcend public link

--- gov-clinic/src/services/articleService.ts
+++ gov-clinic/src/services/articleService.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_URL = process.env.NEXT_PUBLIC_URL || "http://localhost:4000";
+
 export const getAllArticles = async () => {
-    const response = await axios.get("http://localhost:4000/api/articles");
+    const response = await axios.get(`${API_URL}/api/articles`);
 
     return response.data;
 }
