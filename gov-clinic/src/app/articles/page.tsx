5558263d336ad2ae4e5255811ad5f72691926fa9
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-14 17:04:51 +0530

    added article

--- gov-clinic/src/app/articles/page.tsx
+++ gov-clinic/src/app/articles/page.tsx
@@ -1,5 +1,33 @@
 import { useForm } from "react-hook-form";
 
-export default function createArticle() {
-  return ();
+type Article = {
+  title: string,
+  author: string,
+  content: string,
+  readTime: number
+}
+
+export default function CreateArticle() {
+  const form = useForm<Article>();
+  return (
+    <>
+    <form>
+      <label htmlFor="Title">Title</label>
+      <input placeholder="Title" {...form.register("title")}></input>
+      <label htmlFor="Author">Author</label>
+      <input placeholder="Author" {...form.register("author")}></input>
+      <label htmlFor="Content">Content</label>
+      <input placeholder="write your content here" {...form.register("content")}></input>
+      <label htmlFor="readTime">Reading Time</label>
+      <input placeholder="reading time" {...form.register("readTime")}></input>
+
+      <button className="p-6 bg-blue-400 m-10">
+        submit
+      </button>
+      
+      
+    </form>
+
+    </>
+  );
 }
