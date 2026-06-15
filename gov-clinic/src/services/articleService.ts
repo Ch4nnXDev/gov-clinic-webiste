b42dbc5fc6edc58184f215d2f5cb69d5272e4907
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-15 06:01:53 +0530

    added frontend Service for creating article

--- gov-clinic/src/services/articleService.ts
+++ gov-clinic/src/services/articleService.ts
@@ -1,9 +1,22 @@
 import axios from "axios";
 
+type Article = {
+    author: string,
+    title: string,
+    content: string,
+    readTime: number
+}
 const API_URL = process.env.NEXT_PUBLIC_URL || "http://localhost:4000";
 
 export const getAllArticles = async () => {
     const response = await axios.get(`${API_URL}/api/articles`);
 
     return response.data;
+}
+
+
+export const createArticle = async (data: Article) => {
+    const user = await axios.post(`${API_URL}/api/articles`, data);
+    return user;
+    
 }
