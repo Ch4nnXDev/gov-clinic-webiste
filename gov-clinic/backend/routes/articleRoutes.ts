4cb36ceeb79e3185e2340a1361b1fd9b09a4ec37
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-15 05:59:29 +0530

    fixed name error

--- gov-clinic/backend/routes/articleRoutes.ts
+++ gov-clinic/backend/routes/articleRoutes.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { getArticleByIdController, getAllArticlesController, createArticleCOntroller, updateArticleController } from "../controllers/articleController.js";
+import { getArticleByIdController, getAllArticlesController, createArticleController, updateArticleController } from "../controllers/articleController.js";
 
 const router = express.Router();
 
 router.get("/articles/:id", getArticleByIdController);
 router.get("/articles", getAllArticlesController);
-router.post("/articles", createArticleCOntroller);
+router.post("/articles", createArticleController);
 router.put("/articles/:id", updateArticleController);
 
 export default router;
