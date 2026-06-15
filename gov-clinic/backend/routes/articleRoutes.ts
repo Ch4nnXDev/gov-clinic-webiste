45e96591cd26870e81548f489de5972934244f23
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-15 13:48:53 +0530

    fix get to delete

--- gov-clinic/backend/routes/articleRoutes.ts
+++ gov-clinic/backend/routes/articleRoutes.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/articles/:id", getArticleByIdController);
 router.get("/articles", getAllArticlesController);
-router.get("/articles/delete", deleteAllArticlesController);
+router.delete("/articles/delete", deleteAllArticlesController);
 router.post("/articles", createArticleController);
 router.put("/articles/:id", updateArticleController);
 
