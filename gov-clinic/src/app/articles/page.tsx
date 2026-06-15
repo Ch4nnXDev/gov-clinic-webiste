183a00c7cfc28e2f07bb3e76da2f36f74ba9f771
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-15 06:00:35 +0530

    added form submit actions tanStack mutate

--- gov-clinic/src/app/articles/page.tsx
+++ gov-clinic/src/app/articles/page.tsx
@@ -1,5 +1,6 @@
+'use client'
 import { useForm } from "react-hook-form";
-
+import { useCreateArticles } from "@/hooks/getArticles";
 type Article = {
   title: string,
   author: string,
@@ -9,19 +10,45 @@ type Article = {
 
 export default function CreateArticle() {
   const form = useForm<Article>();
+  const createArticle = useCreateArticles();
+  
+  function onSubmit(data: Article){
+    
+    return createArticle.mutate(data);
+
+
+
+  }
+
+  
   return (
     <>
-    <form>
-      <label htmlFor="Title">Title</label>
-      <input placeholder="Title" {...form.register("title")}></input>
-      <label htmlFor="Author">Author</label>
-      <input placeholder="Author" {...form.register("author")}></input>
-      <label htmlFor="Content">Content</label>
-      <input placeholder="write your content here" {...form.register("content")}></input>
-      <label htmlFor="readTime">Reading Time</label>
-      <input placeholder="reading time" {...form.register("readTime")}></input>
+    <form className="p- flex flex-col justify-center items-center m-50 bg-gray-300 shadow:md ">
+      <div className="flex flex-row p-2">
+        <label htmlFor="Title">Title</label>
+        <input placeholder="Title" {...form.register("title")} className="p-4 bg-gray-100"></input>
 
-      <button className="p-6 bg-blue-400 m-10">
+      </div>
+      <div className="flex flex-row p-2">
+        <label htmlFor="Author">Author</label>
+        <input placeholder="Author" {...form.register("author")} className="p-4 bg-gray-100"></input>
+      </div>
+      <div className="flex flex-row p-2">
+        <label htmlFor="Content">Content</label>
+        <input placeholder="write your content here" {...form.register("content")} className="p-4 bg-gray-100"></input>
+      </div> 
+      <div className="flex flex-row p-2">
+         <label htmlFor="readTime">Reading Time</label>
+        <input placeholder="reading time" {...form.register("readTime")} className="p-4 bg-gray-100"></input>
+
+
+      </div> 
+
+      
+      
+      
+     
+      <button className="p-6 bg-blue-400 m-10" onClick={form.handleSubmit(onSubmit)}>
         submit
       </button>
       
