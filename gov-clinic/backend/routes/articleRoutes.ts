a2a443f36a209e55c6627383a08450ef213a92bb
Channa2146@gmail.com <Channa2146@gmail.com>
2026-06-15 09:02:01 +0530

    add delete all route

--- gov-clinic/backend/routes/articleRoutes.ts
+++ gov-clinic/backend/routes/articleRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { getArticleByIdController, getAllArticlesController, createArticleController, updateArticleController } from "../controllers/articleController.js";
+import { getArticleByIdController, getAllArticlesController, createArticleController, updateArticleController, deleteAllArticlesController } from "../controllers/articleController.js";
 
 const router = express.Router();
 
 router.get("/articles/:id", getArticleByIdController);
 router.get("/articles", getAllArticlesController);
+router.get("/articles/delete", deleteAllArticlesController);
 router.post("/articles", createArticleController);
 router.put("/articles/:id", updateArticleController);
 
